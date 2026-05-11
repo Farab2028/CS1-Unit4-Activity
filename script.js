3aec3c55aa19317e69f9db12a5ee4c27a48eb75c
Farab2028 <fara.belitz2028@gbwl.org>
2026-05-11 12:55:18 +0000

    added another button

--- script.js
+++ script.js
@@ -18,3 +18,37 @@ heading.style.color = "purple"; // JS wrote CSS code...
 heading.textContent = "Light Mode & Dark Mode Webstie" ;
 
 
+
+// REVIEW
+// 1. Select the new element on the page (by id name)
+const pastelBtn = document.querySelector("#pastel");
+// 2. Attach click event listener to the button
+pastelBtn.addEventListener("click", setPastel);
+// 3. Define the function that triggers when clicked
+function setPastel() {
+   console.log("setting pastel theme...");
+   body.style.background = "rgb(0, 0, 0)";
+   body.style.color = "rgb(9, 225, 83)";
+   body.style.fontFamily = "monospace";
+   description.textContent = "WELCOME TO PASTEL MODE!";
+}
+
+// TOGGLE between light & dark mode
+// 2. attach event listener
+toggleBtn.addEventListener("click", toggleMode);
+// 3. define the function
+function toggleMode() {
+   darkMode = !darkMode; // flip the boolean
+   console.log(darkMode);
+   if (darkMode == true) {
+      console.log("apply dark mode styles");
+
+   }
+   else {
+      console.log("apply light mode styles");
+
+   }
+}
+
+
+
